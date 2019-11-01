680540eedebf17740112cd61086b0649e857340e
- HOTD T H 17 <thaales_jp@hotmail.com>
2019-11-01 09:23:41 -0300

    #Implementando Template Method

---
+++ DesignPatterns/TemplateDeImpostoCondicional.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public abstract class TemplateDeImpostoCondicional : Imposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (DeveUsarMaximaTaxacao(orcamento))
+            {
+                return MaximaTaxacao(orcamento);
+            }
+            return MinimaTaxacao(orcamento);
+        }
+
+        public abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
+        public abstract double MaximaTaxacao(Orcamento orcamento);
+        public abstract double MinimaTaxacao(Orcamento orcamento);
+
+    }
+}
